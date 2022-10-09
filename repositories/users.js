93e9715782e78d0c9eea5898b51cf1d8c6bc01cc
chiesad1 <chiesad1@gmail.com>
2022-10-08 22:47:58 -0400

    validation logic

--- repositories/users.js
+++ repositories/users.js
@@ -64,13 +64,24 @@ class UserRepository {
 
         await this.writeAll(records);
     }
+
+    async getOneBy(filters){
+        const records = await this.getAll();
+
+        for (let record of records){
+            let found = true;
+            
+            for (let key in filters){
+                if (record[key] !== filters[key]){
+                    found = false;
+                }
+            }
+
+            if (found){
+                return record;
+            }
+        }
+
+    }
 }
-const test = async () => {
-    const repo = new UserRepository('users.json');
-
-    // await repo.create({email: 'dsasda@dasdsdsadsasdsadsad.com'});
-
-    await repo.update('03277d12', {password: 'myPassword'});
-};
-
-test();
+module.exports = new UserRepository('users.json');
